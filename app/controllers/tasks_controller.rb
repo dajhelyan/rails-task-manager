4bcc373d3da84ca7486b7fc36fb2a545fa0d0b17
dajhely angles <assiaxramx10@gmail.com>
2021-09-29 10:55:26 -0500

    impementing the creation of a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,33 @@
 class TasksController < ApplicationController
+  # return all taks from db Task
   def index
     @tasks = Task.all
   end
+  # return a task for id
   def show
     @tasks = Task.find(params[:id])
   end
+  # create a task instance
   def new
+    # create
+    @task = Task.new
+    
+  end
+
+  def create
+    @task = Task.new(task_params)
+    # save the task instance
+    if @task.save
+      # redirect to the tasks views
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
